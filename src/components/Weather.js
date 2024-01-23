ff1c4bdc722b99dc1fc3200c881ca16eaa2d83aa
gabmora <gabimm29@gmail.com>
2024-01-23 18:30:02 -0500

    openweathermap api

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,19 @@ import Inputs from './weather_comps/Inputs';
 import TimeAndLocation from './weather_comps/TimeAndLocation';
 import TemperatureAndDetails from './weather_comps/TemperatureAndDetails';
 import Forecast from './weather_comps/Forecast';
+import getFormattedWeatherData from './weather_comps/services_weather/weatherService';
 
 
 function Weather(){
 
+
+
+    const fetchWeather = async () => {
+        const data = await getFormattedWeatherData( {q: "dallas" });
+        console.log(data);
+    };
+
+    fetchWeather();
 return(
     <div>
         <TopButtons />
---
+++ src/components/weather_comps/services_weather/weatherService.js
@@ -0,0 +1,103 @@
+import { DateTime } from "luxon";
+
+const API_KEY = 'd2888d37664143799cccb1ffd7200076'
+const BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+// https://api.openweathermap.org/data/2.5/weather?q=Dallas,usa&APPID=d2888d37664143799cccb1ffd7200076
+
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL + "/" + infoType);
+    url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  
+    return fetch(url).then((res) => res.json());
+  };
+  
+  const formatCurrentWeather = (data) => {
+    const {
+      coord: { lat, lon },
+      main: { temp, feels_like, temp_min, temp_max, humidity },
+      name,
+      dt,
+      sys: { country, sunrise, sunset },
+      weather,
+      wind: { speed },
+    } = data;
+  
+    const { main: details, icon } = weather[0];
+  
+    return {
+      lat,
+      lon,
+      temp,
+      feels_like,
+      temp_min,
+      temp_max,
+      humidity,
+      name,
+      dt,
+      country,
+      sunrise,
+      sunset,
+      details,
+      icon,
+      speed,
+    };
+  };
+  
+  const formatForecastWeather = (data) => {
+    let { timezone, daily, hourly } = data;
+
+    // Check if 'hourly' is undefined or not an array
+    if (!hourly || !Array.isArray(hourly)) {
+        console.error("Hourly data is undefined or not an array in formatForecastWeather function.");
+        return { timezone, daily: [], hourly: [] };
+    }
+
+    // Check if the array has at least 6 elements before slicing
+    if (hourly.length >= 6) {
+        hourly = hourly.slice(1, 6).map((d) => {
+            return {
+                title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+                temp: d.temp,
+                icon: d.weather[0].icon,
+            };
+        });
+    } else {
+        console.warn("Hourly data does not have enough elements in formatForecastWeather function.");
+    }
+
+    return { timezone, daily: [], hourly };
+};
+
+  
+  const getFormattedWeatherData = async (searchParams) => {
+    const formattedCurrentWeather = await getWeatherData(
+      "weather",
+      searchParams
+    ).then(formatCurrentWeather);
+  
+    const { lat, lon } = formattedCurrentWeather;
+  
+    const formattedForecastWeather = await getWeatherData("onecall", {
+      lat,
+      lon,
+      exclude: "current,minutely,alerts",
+      units: searchParams.units,
+    }).then(formatForecastWeather);
+  
+    return { ...formattedCurrentWeather, ...formattedForecastWeather };
+  };
+  
+  const formatToLocalTime = (
+    secs,
+    zone,
+    format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+  ) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+  
+  const iconUrlFromCode = (code) =>
+    `http://openweathermap.org/img/wn/${code}@2x.png`;
+  
+  export default getFormattedWeatherData;
+  
+  export { formatToLocalTime, iconUrlFromCode };
